a4e0895f796163dcfd484c63d16d54874842e0bd
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2019-10-22 15:54:59 -0500

    Updated filtering functionality to better keep up with async.

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -4,9 +4,16 @@ export default Controller.extend({
   actions: {
     filterByCity(param) {
       if (param !== '') {
-        return this.store.query('rental', { city: param });
-      } else {
-        return this.store.findAll('rental');
+		return this.store
+          .query('rental', { city: param }).then((results) => {
+            return { query: param, results: results };
+          });      
+      } 
+      else {
+        return this.store
+          .findAll('rental').then((results) => {
+            return { query: param, results: results };
+          });      
       }
     }
   }	
