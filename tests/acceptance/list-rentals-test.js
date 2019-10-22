7dec995d53c516c3c1ebe59ff3b8ea5552bfca21
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2019-10-22 16:12:05 -0500

    Improved Acceptance tests.

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,7 +4,9 @@ import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import {
   click,
   currentURL,
-  visit
+  visit,
+  fillIn,
+  triggerKeyEvent
 } from '@ember/test-helpers';
 
 module('Acceptance | list rentals', function(hooks) {
@@ -17,15 +19,15 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should link to information about the company', async function(assert) {
-	await visit('/');
-	await click(".menu-about");
-	assert.equal(currentURL(), '/about', 'should navigate to about')
+  	await visit('/');
+  	await click(".menu-about");
+  	assert.equal(currentURL(), '/about', 'should navigate to about')
   });
 
   test('should link to contact information', async function(assert) {
-	await visit('/');
-	await click(".menu-contact");
-	assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  	await visit('/');
+  	await click(".menu-contact");
+  	assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals.', async function(assert) {
@@ -34,6 +36,11 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should filter the list of rentals by city.', async function (assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
 
   test('should show details for a selected rental', async function (assert) {
